5a3ffe602421c344ed86f2d5f19e7b77d1cb5349
Hanh Solo <minhhanhnyc@gmail.com>
2021-01-20 14:24:19 -0800

    Video lib

--- src/components/VideoLib.js
+++ src/components/VideoLib.js
@@ -1,10 +1,43 @@
-import React,  from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const VideoLib = () => (
+const LIBRARY_URL = 'http://localhost:3000/videos'
+
+const VideoLib = () => {
+
+  const [videoList, setVideoList] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    axios.get(LIBRARY_URL)
+    .then((response) => {
+      // Get the list of videos
+      setVideoList(response.data)
+    })
+    .catch((error) => {
+      setErrorMsg(error.message);
+      console.log(error.message);
+    })
+  }, []);
+
+  const videoListView = videoList.map( video => {
+    return (
+      <div>
+        <h3>{video.title}</h3>
+        <p>id: {video.id}</p>
+        <p>overview: {video.overview}</p>
+        <p>release_date: {video.release_date}</p>
+        <img src={video.image_url}  alt =''/> 
+      </div>
+    )
+  });
+  return(
     <div className='library'>
+      { errorMsg ? <div><h2 className='error-msg'>{errorMsg}</h2></div> : '' }
       <h1>Libarian needed</h1>
- 
+      { videoListView }
     </div>
   );
+}
 
 export default VideoLib;
