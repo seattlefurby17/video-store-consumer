97509a4e0e505d2d154395101a7c8de8d01ef126
Hanh Solo <minhhanhnyc@gmail.com>
2021-01-20 17:46:02 -0800

    Video search updated

--- src/components/VideoSearch.js
+++ src/components/VideoSearch.js
@@ -1,14 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import VideoSearchForm from './VideoSearchForm';
+import VideoSearchResult from './VideoSearchResult';
 
 const VIDEO_URL = 'http://localhost:3000/videos?query='
 
-const VideoSearch = () => (
+const VideoSearch = () => {
+  
+  const [videoSearchList, setVideoSearchList] = useState([])
+  const [errorMsg, setErrorMsg] = useState(null);
 
+  const searchVideos = (title) => {
+    axios.get(`${VIDEO_URL}${title}`)
+    .then((response) => {
+      setVideoSearchList(response.data);
+    })
+    .catch((error) => {
+      setErrorMsg(error.message);
+      console.log(error.message);
+    })
+  }
+
+  const videoSearchView = videoSearchList.map( video => {
+    return (
+      <VideoSearchResult video = {video}/>
+    )
+  });
+
+  return (
     <div className='Search'>
-      <h1>Who's there?</h1>
- 
+      <VideoSearchForm searchVideoCallback = {searchVideos}/>
+      { errorMsg ? <div><h2 className='error-msg'>{errorMsg}</h2></div> : '' }
+      {videoSearchView}
     </div>
-  );
+  )};
 
 export default VideoSearch;
---
+++ src/components/VideoSearchResult.js
@@ -0,0 +1,32 @@
+import React,  {useState } from 'react';
+import axios from 'axios';
+
+const LIBRARY_URL = 'http://localhost:3000/videos'
+
+const VideoSearchResult = (props) => {
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [successMsg, setSuccessMsg] = useState(null);
+    const onButtonClick = () => {
+        axios.post(LIBRARY_URL, {inventory: 1, ...props.video})
+        .then((response) => {
+          setSuccessMsg('Video Added!')
+    })
+        .catch((error) => {
+        setErrorMsg(error.message);
+        console.log(error.message);
+    })
+  }
+  
+  return (
+    <div>
+      <h3>{props.video.title}</h3>
+      <p>overview: {props.video.overview}</p>
+      <p>release_date: {props.video.release_date}</p>
+      <img src={props.video.image_url}  alt =''/> 
+      {successMsg ? successMsg : errorMsg}
+      <button onClick ={onButtonClick}>Add Movie to Library</button>
+    </div>
+  )
+}
+
+export default VideoSearchResult;
